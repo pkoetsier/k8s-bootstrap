4988b2339bbcbff446e79ec2f36e33f778879fb1
Paul Koetsier <pkoetsier.external@epo.org>
2023-04-29 08:33:06 +0200

    Initial commit

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ It has these prequisites:
 - `inventory.yaml` contains the list of VMs which will be provisioned, their IP addresses and the K8S version. Adapt it to your needs.
 - `bootstrap.sh` is used to provisioned the VMs and bootstrap the K8S cluster.
 It takes care of installing the prequisite Debian software packages and Ansible Galaxy roles.
+
+Following the bootstrap, the .ssh directory will contain a SSH keypair which can be used to login to the VMs like so:
+
+```
+ssh -i .ssh/ansible -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ansible@<node>
+```
+
+The ansible user has full `NOPASSWD` Sudo access.
---
+++ bootstrap.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+
+PREREQ_PACKAGES="ansible"
+
+function msg {
+    level=0
+
+    (($# > 1)) && { level="${1}"; shift; }
+    ((verbose >= level)) || return
+        echo -e "INFO $(date) ${1}"
+}
+
+function warn {
+    echo -e "WARNING $(date) ${1}" >&2
+    ((errors++))
+}
+
+function err {
+    echo -e "ERROR $(date) ${1}" >&2
+    ((errors++))
+}
+
+function die {
+    rc=1
+
+    (($# > 1)) && { rc="${1}"; shift; }
+    err "${1}"
+
+    exit ${rc}
+}
+
+function install_prereqs {
+  for package in ${PREREQ_PACKAGES}; do
+    dpkg -l ${package} >/dev/null 2>&1 || sudo apt install -y ${package} || die "failed to install ${package}"
+  done
+}
+
+function install_ansible_galaxy_roles {
+  [ -e galaxy_roles.yaml ] || die "cannot find galaxy_roles.yaml"
+  ansible-galaxy role install -r galaxy_roles.yaml || die "failed to install Ansible Galaxy roles"
+
+}
+
+function passwd_hash {
+  echo -n ansible | openssl passwd -6 -stdin
+}
+
+function create_ssh_key {
+  [ -e .ssh/ansible ] && return 0
+
+  [ -d .ssh ] || mkdir .ssh || die "failed to create .ssh directory"
+  ssh-keygen -b 2048 -N "" -f .ssh/ansible || die "failed to create ansible SSH keypair"
+}
+
+function install_k8s_cluster {
+   ansible-playbook -i inventory.yaml install_vms.yaml || die "failed to install VMs"
+   sleep 60
+   ansible-playbook -i inventory.yaml install_kubernetes.yaml || die "failed to install K8S cluster"
+}
+
+# Main
+install_prereqs
+install_ansible_galaxy_roles || die "failed to install the needed Ansible galaxy roles"
+create_ssh_key
+#passwd_hash
+install_k8s_cluster
